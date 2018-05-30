0f40fb42676ec7509be58033dbccdb4a5bcaff4b
Yusha Wang <sophiawang1106@hotmail.com>
2018-05-30 00:17:06 -0700

    yusha part finished

--- my_server.R
+++ my_server.R
@@ -1,7 +1,112 @@
+library(shiny)
+library(dplyr)
+library(ggplot2)
+library(tidyr)
+library(lubridate)
+library(parsetR)
+
+
 trip_data <- read.csv("data/trip.csv", stringsAsFactors = FALSE)
 weather_data <- read.csv("data/weather.csv", stringsAsFactors = FALSE)
 station_data <- read.csv("data/station.csv", stringsAsFactors = FALSE)
 
 my_server <- function(input,output) {
+  #####################################yusha section####################################################
+  #filter useful data information
+  yusha_small_data <- trip_data %>% 
+    select(starttime, from_station_name, to_station_name, from_station_id, to_station_id)
+  #convert into datetime format
+  yusha_small_data$starttime <- mdy_hm(yusha_small_data$starttime)
   
+  yusha_small_data$starttime <- format(yusha_small_data$starttime, "%Y-%m-%d")
+  yusha_small_data$starttime <- ymd(yusha_small_data$starttime)
+  #extract year
+  yusha_small_data$year <- year(yusha_small_data$starttime)
+  #extract month
+  yusha_small_data$month <- month(yusha_small_data$starttime)
+  
+  
+  output$secondSelection <- renderUI({
+    selectInput("month", "Pick a Month:", choices = sort(unique(yusha_small_data$month[yusha_small_data$year ==input$year])))
+  })
+  
+  # Assign a reactive function to the outputted filtered data frame value to produce a table
+  yusha_table <- reactive({
+    yusha_small_datatemp <- yusha_small_data %>%
+      filter(year == input$year & month == input$month ) %>% 
+      dplyr::group_by(from_station_name,  to_station_name, from_station_id, to_station_id) %>%
+      dplyr::summarize(counts = n()) %>% 
+      ungroup() %>%
+      arrange(-counts)
+    return(yusha_small_datatemp)
+  })
+  
+  #Assign a reactive function to the outputted filtered data frame value to produce a summary table
+  yusha_summary <- reactive({
+    yusha_small_datatemp <- yusha_small_data %>%
+      filter(year == input$year & month == input$month & from_station_id != to_station_id) %>% 
+      dplyr::group_by(from_station_name,  to_station_name, from_station_id, to_station_id) %>%
+      dplyr::summarize(counts = n()) %>%
+      ungroup() %>%
+      arrange(-counts) %>% 
+      filter(counts == max(counts)) 
+    return(yusha_small_datatemp)
+  })
+  
+  # Assign a reactive function to the outputted filtered data frame value to produce a plot
+  yusha_filtered <- reactive({
+    yusha_small_datatwo <- yusha_small_data %>%
+      filter(year == input$year & month == input$month & from_station_id != to_station_id ) %>% 
+      dplyr::group_by(from_station_id, to_station_id) %>%
+      dplyr::summarize(counts = n()) 
+    #filter better data set
+    if(input$year == 2014){
+      yusha_small_datatemp <- yusha_small_datatwo %>% 
+        filter(counts >= 30) 
+    }
+    if(input$year == 2015){
+      if(input$month %in% c(12,11)){
+        yusha_small_datatemp <- yusha_small_datatwo %>% 
+          filter(counts >= 33)
+      }
+      if(input$month %in% c(5,6,7,8)){
+        yusha_small_datatemp <- yusha_small_datatwo %>% 
+          filter(counts >= 85)
+      }
+      if(input$month %in% c(1,2,3,4,9,10)){
+        yusha_small_datatemp <- yusha_small_datatwo %>% 
+          filter(counts >= 60)
+      }
+    }
+    if(input$year == 2016){
+      if(input$month %in% c(5,6,7,8)){
+        yusha_small_datatemp <- yusha_small_datatwo %>% 
+          filter(counts >= 55)
+      }
+      if(input$month %in% c(1,2,3,4)){
+        yusha_small_datatemp <- yusha_small_datatwo %>% 
+          filter(counts >= 40)
+      }
+    }
+    
+    yusha_small_datatemp <-  yusha_small_datatemp %>% 
+      ungroup() %>%
+      arrange(-counts)
+    return(yusha_small_datatemp)
+  })
+  
+  
+  #output plot with mean eviction rate versus states according to user input
+  output$plot <- renderParset({
+    parset(data = yusha_filtered(), dimensions = c('from_station_id' , 'to_station_id'), 
+           value = htmlwidgets::JS("function(d){return d.counts}"),
+           tension = 0.5)
+    
+  })
+  #output data table
+  output$view <- renderDataTable(yusha_table())
+  
+  #output summary table
+  output$max <- renderTable(yusha_summary())
+  ######################################################################################################
 }
--- my_ui.R
+++ my_ui.R
@@ -1,11 +1,83 @@
+
+library(shiny)
+library(dplyr)
+library(ggplot2)
+library(tidyr)
+library(lubridate)
+library(parsetR)
+
+trip_data <- read.csv("data/trip.csv", stringsAsFactors = FALSE)
+weather_data <- read.csv("data/weather.csv", stringsAsFactors = FALSE)
+station_data <- read.csv("data/station.csv", stringsAsFactors = FALSE)
+########################yusha_data_frame#################################
+#filter useful data information
+yusha_small_data <- trip_data %>% 
+  select(starttime, from_station_name, to_station_name, from_station_id, to_station_id)
+#convert into datetime format
+yusha_small_data$starttime <- mdy_hm(yusha_small_data$starttime)
+yusha_small_data$starttime <- format(yusha_small_data$starttime, "%Y-%m-%d")
+yusha_small_data$starttime <- ymd(yusha_small_data$starttime)
+#extract year
+yusha_small_data$year <- year(yusha_small_data$starttime)
+#extract month
+yusha_small_data$month <- month(yusha_small_data$starttime)
+###########################################################################
 my_ui <- navbarPage(
   "Our Title",
   tabPanel(
     "Introduction"
   ),
   tabPanel(
-    "Yusha"
-  ), # Yusha
+    "Popular Trip",
+    titlePanel("Bicycle Trip in Seattle"),
+    sidebarLayout(
+      sidebarPanel(
+        selectInput("year",
+                    "Pick a Year:",
+                    c(unique(yusha_small_data$year))),
+        # br() element to introduce extra vertical spacing
+        br(),
+        uiOutput("secondSelection"),
+        
+        #selectInput("month",
+        #  "Pick a month:",
+        # c(sort(unique(yusha_small_data$month)))),
+        
+        # Include clarifying text 
+        helpText("Note:", strong("only")," trips have relatively large passengers (counts) and with different origine and destination
+                 are plotted under the graph tab. 
+                 But ", strong("all")," observed trips will be displayed under the table tab.") 
+        ),
+      mainPanel(
+        tabsetPanel(
+          tabPanel("Graph", 
+                   h2("What is the most popular trip?"),
+                   p("In this section, we will explore bicycle trip data in Seattle area from 2014-10 to 2016-08. We only look 
+                     at start points and end points, so the path is unknown. However, as long as bikers have the same origin
+                     and destinition, we treat them as the same geoup. You can select which year and which month's biking trips you want to see. 
+                     The graph will produce the total number of biker traveling between each stations. If you click on the graph, we can see the total number of biker travelled 
+                     between specific stations and the percentage of bikers taking that trips. Stations' id are displayed, but you can
+                     find the full name by checking the table data under the table tab.Only relatively large number of bikers in trips are 
+                     shown. However, you can always check the table for full data."),
+                   br(),
+                   parsetOutput("plot"),
+                   br(),
+                   h3(strong("The table belows shows the trip(s) that has maximun number or passengers for the selected year and month: ")),
+                   tableOutput("max"),
+                   br(),
+                   h4("Trip that has different origin and destinition with the largest number of passengers is the
+                      most popular trip (check the above table). So more stations can be settled nearby allowing large number of bicycle to park. The company may gain some profit from this."),
+                   
+                   br()
+                   ),
+          tabPanel("table",
+                   dataTableOutput("view")
+          )
+          
+          )
+        )
+        )
+      ), # Yusha
   tabPanel(
     "Jin"
   ), # Jin
