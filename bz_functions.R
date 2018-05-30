c1493e26109b37dd22026c0eeff0ebe3c918e81b
Benjamin Zielinski <bwz@uw.edu>
2018-05-30 12:58:24 -0700

    Add comments

--- bz_functions.R
+++ bz_functions.R
@@ -1,7 +1,10 @@
+# Takes a string and returns the characters up to the first space
 bz_get_first_word <- function(x) {
   strsplit(x, " ")[[1]][1]
 }
 
+# Takes a string and gets the characters up to the first slash "/", then
+#   converts those characters into a numeric. Returns the resulting number
 bz_get_month <- function(x) {
   p <- strsplit(x, "/")[[1]][1]
   as.numeric(p)
--- my_server.R
+++ my_server.R
@@ -2,6 +2,7 @@ trip_data <- read.csv("data/trip.csv", stringsAsFactors = FALSE)
 weather_data <- read.csv("data/weather.csv", stringsAsFactors = FALSE)
 station_data <- read.csv("data/station.csv", stringsAsFactors = FALSE)
 
+# BZ: I need to source in some functions to sapply over a vector
 source("bz_functions.R")
 
 my_server <- function(input,output) {
